2022c17e98be25a13d8aa46b661a94a9e96f145b
Hab1deen <habideen1111@gmail.com>
2026-02-19 11:11:24 +0700

    Port 465 (SSL) + IPv4 + Type Casting

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -9,8 +9,8 @@ class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
